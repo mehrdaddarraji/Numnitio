90b537e073777fb431ce94260456e7dcd84240ad
mehrdaddarraji <mehrdad_da@yahoo.com>
2018-08-09 03:21:41 -0500

    string request for registeration

--- app/src/main/java/com/darraji/mehrdad/numnitio/RegisterActivity.java
+++ app/src/main/java/com/darraji/mehrdad/numnitio/RegisterActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -21,13 +22,14 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
     private EditText etName, etRegisterEmail, etRegisterPassword, etRegisterConfirmPassword;
     private Button bRegister;
     private ProgressBar pbRegisterLoading;
-    private static String registerUrl = "";
+    private static String registerUrl = "http://numnitio.000webhostapp.com/Register.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,14 +85,31 @@ public class RegisterActivity extends AppCompatActivity {
                 Response.Listener<String> responseListener = new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            String success = jsonResponse.getString("success");
+                            if(success.equals("1")) {
+                                Toast.makeText(RegisterActivity.this, "Register Successful!",
+                                        Toast.LENGTH_SHORT).show();
+                            }
 
+                        } catch(JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(RegisterActivity.this, "Register Failed! "
+                                    + e.toString(), Toast.LENGTH_SHORT).show();
+                            pbRegisterLoading.setVisibility(View.GONE);
+                            bRegister.setVisibility(View.VISIBLE);
+                        }
                     }
                 };
 
                 Response.ErrorListener responseErrorListener = new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-
+                        Toast.makeText(RegisterActivity.this, "Register Failed! "
+                                + error.toString(), Toast.LENGTH_SHORT).show();
+                        pbRegisterLoading.setVisibility(View.GONE);
+                        bRegister.setVisibility(View.VISIBLE);
                     }
                 };
 
@@ -98,9 +117,16 @@ public class RegisterActivity extends AppCompatActivity {
                         responseListener, responseErrorListener) {
                     @Override
                     protected Map<String, String> getParams() throws AuthFailureError {
-                        return super.getParams();
+                        Map<String, String> params = new HashMap<>();
+                        params.put("name", name);
+                        params.put("email", email);
+                        params.put("password", password);
+                        return params;
                     }
                 };
+
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(stringRequest);
 
             }
         });
